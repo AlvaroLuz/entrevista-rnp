9b2ee9ed0117cb6d2c80ca72a5a04d68c3261d32
AlvaroLuz <alvaroluz2001@gmail.com>
2025-11-16 01:39:14 -0300

    adição de comentário e ajuste para prevenção de bug

--- grafana/grafana-entrypoint.sh
+++ grafana/grafana-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
-export INFLUX_TOKEN=$(cat ${INFLUX_TOKEN_FILE})
+# Carregando a variavel de ambiente INFLUX_TOKEN a partir do arquivo de segredo
+# usado no datasources.yaml do grafana para conectar no InfluxDB
+export INFLUX_TOKEN=$(cat $INFLUX_TOKEN_FILE)
 
 exec /run.sh
